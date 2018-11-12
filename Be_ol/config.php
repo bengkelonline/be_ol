1a83d4b5c931d93411b7ee0e7285c8032e83a801
mohammadsyafrianabie <43847865+mohammadsyafrianabie@users.noreply.github.com>
2018-11-12 12:22:37 +0700

    Add files via upload

---
+++ Be_ol/config.php
@@ -0,0 +1,8 @@
+<?php
+	$host		= "localhost";
+	$user		= "root";
+	$password	= "";
+	$name		= "be_ol";
+
+	$koneksi= mysqli_connect($host,$user,$password,$name) or die ("koneksi ke database gagal")
+?>
